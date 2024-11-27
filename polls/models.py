570028e7d6c78373958df0ff83b0d069a8ba83e9
Ishana Jain <ishanajain99@gmail.com>
2024-11-27 15:40:29 -0700

    Finished models - missing Main

--- polls/models.py
+++ polls/models.py
@@ -13,11 +13,42 @@ class User(models.Model):
     password = models.CharField(max_length=255)
 
 class Profile(models.Model):
-    user = user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, related_name='profile')
+    user  = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, related_name='profile')
     logout = models.BooleanField(default=False)
     address = models.CharField(max_length=255, blank=True, null=True)
     phone = models.CharField(max_length=20, blank=True, null=True)
     email = models.EmailField(max_length=255, blank=True, null=True)
+
+class Item(models.Model):
+    itemID = models.AutoField(primary_key=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    cloth = models.BooleanField(default=False)
+    equip = models.BooleanField(default=False)
+    text = models.BooleanField(default=False)
+    condition = models.CharField(max_length=100, choices=[('New', 'New'), ('Used', 'Used')])
+    image = models.TextField(blank=True, null=True)
+
+class Filter(models.Model):
+    item = models.OneToOneField(Item, on_delete=models.CASCADE, related_name='filter')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    cloth = models.BooleanField(default=False)
+    equip = models.BooleanField(default=False)
+    condition = models.CharField(max_length=100, choices=[('New', 'New'), ('Used', 'Used')])
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cart_items')
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='cart_entries')
+    image = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    payment = models.CharField(max_length=255, choices=[ ('Completed', 'Completed'), ('NoPayment', 'NoPayment')])
+
+class Login(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='login')
+    email = models.EmailField(max_length=255)
+    password = models.CharField(max_length=255)
+
 
 
 
