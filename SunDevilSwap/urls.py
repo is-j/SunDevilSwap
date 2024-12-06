347ea2b8c258ec6509003be1403b5dc64e997fff
Ishana Jain <ishanajain99@gmail.com>
2024-12-05 17:18:08 -0700

    Added html files for pages

--- SunDevilSwap/urls.py
+++ SunDevilSwap/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('main/', include("mainApp.urls")),
     path('admin/', admin.site.urls),
+    path('login/', include("mainApp.urls") )
 ]
  
