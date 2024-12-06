e32e821da9b339502438c463924b27dddeafb18d
Ishana Jain <ishanajain99@gmail.com>
2024-12-05 20:23:30 -0700

    Login and Signin page

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.login_view, name='login'),
+    path('signup/', views.signUp_view, name='signup'),
+   # path('sell/', views.sell_item, name='sell_item'),
     
 ]
